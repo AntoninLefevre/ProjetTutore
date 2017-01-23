ecc77424cb6e07339d44e88ee66fb0d52ba7083a
AntoninLefevre <antonin_08430@hotmail.fr>
2017-01-23 21:32:08 +0100

    Classe WebPage réalisée

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once('autoload.inc.php');
+
+$wp = new WebPage("Page d'accueil");
+
+$wp->appendContent(<<<HTML
+    <h1>Test de la classe WebPage</h1>
+HTML
+);
+
+echo $wp->toHTML();
